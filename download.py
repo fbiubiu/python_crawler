57b2f03ed17a56785e5517868c70eba9860cd849
obelisk.feng <obelisk.feng@zhan.com>
2018-03-14 19:54:31 +0800

    sitemap爬取实例

--- download.py
+++ download.py
@@ -1,10 +1,11 @@
 import urllib.request
 import re
-import urlparse
+from urllib.parse import urljoin
 
 
+# 页面下载方法
 def download(url, user_agent='wswp', num_retires=2):
-    print('Downloading:', url)
+    # print('Downloading:', url)
     headers = {'User-agent': user_agent}  #设置用户代理
     request = urllib.request.Request(url, headers=headers)
     try:
@@ -17,20 +18,35 @@ def download(url, user_agent='wswp', num_retires=2):
                 return download(url, user_agent, num_retires-1)
     return html
 
-def link_crawler(seed_url, link_regex):
-    crawl_queue = [seed_url]  #要爬取的url列表
-    seen = set(crawl_queue)
-    while crawl_queue:
-        url = crawl_queue.pop()
-        html = download(url)  #下载获取页面
-        for link in get_links(html):  #对获取到的页面链接使用正则进行筛选
-            if re.match(link_regex, link):
-                link = urlparse.urljoin(seed_url, link) #解析链接link，转化为绝对连接
-                if link not in seen:
-                    seen.add(link)
-                    crawl_queue.append(link)
 
-#正则匹配获取页面链接
+# 链接爬取方法
+def link_crawler(seed_url, link_regex, max_depth=5):
+    crawl_queue = [seed_url]  # 要爬取的url列表
+    seen = {seed_url: 0}  # 已爬取网站列表
+    file_obj = open('/usr/local/nginx/html/try/crawler/url_list.txt', 'a+')
+    try:
+        while crawl_queue:
+            url = crawl_queue.pop()
+            html = download(url)  # 下载获取页面
+            depth = seen[url]  # 获取当前页面深度
+            if depth != max_depth:
+                for link in get_links(html):  # 对获取到的页面链接使用正则进行筛选
+                    link = urljoin(seed_url, link).strip()  # 解析链接link，转化为绝对连接
+                    if re.match(link_regex, link):
+                        if link not in seen:
+                            print(link)
+                            file_obj.write(link+'\n')
+                            seen[link] = depth + 1
+                            crawl_queue.append(link)
+    finally:
+        file_obj.close()
+
+
+# 正则匹配获取页面链接
 def get_links(html):
     webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
-    return webpage_regex.findall(html)
+    return webpage_regex.findall(html.decode('utf-8'))
+
+
+link_crawler('https://top.zhan.com', '^(http(s)?)://top.zhan.com(?!/cihui)[^?]*$')
+# print(re.match('^(http(s)?)://top.zhan.com(?!/cihui).*$','https://top.zhan.com/cihui/ielts-decline.html'))
